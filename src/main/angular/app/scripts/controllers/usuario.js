be96e937ccd594a0e1d8bbac6dc2fe24b1dbaf93
Diego Drumond <diegodrumond@hotmail.com>
2015-10-25 15:32:04 -0200

    merge do arquivo mapa.js

--- src/main/angular/app/scripts/controllers/usuario.js
+++ src/main/angular/app/scripts/controllers/usuario.js
@@ -2,6 +2,7 @@
 
 angular.module('c3aApp')
 	.controller('UsuarioCtrl', function ($scope, Session) {
+		console.log(Session);
 		$scope.user = {name: Session.name, login: Session.login};
 		$scope.menu =  {name: 'menu', url: 'views/menu.html'};
 	});
